06d63ea49a02fe57de46bdd7e23f767bb6d3b80c
IgniFyris <ignifyris@gmail.com>
2026-04-06 17:42:45 +0800

    feat: add main menu music function and update splash screen text

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -11,6 +11,8 @@ func _ready() -> void:
 
 func _on_begin_pressed() -> void:
 	SceneLoader.load_scene("uid://bbxwnl4ctuot6", 1.5)
+	var tween = create_tween().tween_property(MainMenuMusic, "volume_db", -90.0, 3)
+	await tween.finished
 
 func _on_options_pressed() -> void:
 	var options = options_scene.instantiate()
--- Scripts/splash_screen.gd
+++ Scripts/splash_screen.gd
@@ -6,20 +6,22 @@ var width
 var height
 
 func _ready() -> void:
+	MainMenuMusic.play()
 	width = get_viewport_rect().size.x
 	height = get_viewport_rect().size.y
 	CLText.modulate.a = 0
 	CLLogo.modulate.a = 1
 	
 	CLLogo.position = Vector2(width/2, height/2)
+	CLText.position = Vector2(width/2, height/2)
 
-	var LogoFadeOut = create_tween().tween_property(CLLogo, "modulate:a", 0, 1).set_delay(2)
+	var LogoFadeOut = create_tween().tween_property(CLLogo, "modulate:a", 0, 2.7).set_delay(2).set_ease(Tween.EASE_IN_OUT)
 	await LogoFadeOut.finished
 	
-	var TextFadeIn = create_tween().tween_property(CLText, "modulate:a", 1, 1)
+	var TextFadeIn = create_tween().tween_property(CLText, "modulate:a", 1, 3).set_ease(Tween.EASE_IN_OUT)
 	await TextFadeIn.finished
 	
-	var TextFadeOut = create_tween().tween_property(CLText, "modulate:a", 0, 1).set_delay(2)
+	var TextFadeOut = create_tween().tween_property(CLText, "modulate:a", 0, 2.6).set_delay(2).set_ease(Tween.EASE_IN_OUT)
 	await TextFadeOut.finished
 	
 	SceneLoader.load_scene("uid://cvp7sk28csjfr", 0)
