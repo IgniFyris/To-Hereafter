adde5726d2f2a6061fc530874d0f10e04ccedaa7
IgniFyris <ignifyris@gmail.com>
2026-04-11 22:04:40 +0800

    feat: add basic memory fragment functionality

--- Scripts/Level Generation/memory_fragment.gd
+++ Scripts/Level Generation/memory_fragment.gd
@@ -2,4 +2,14 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
-		pass #triggers memorythingy (to be added)
+		body.memoryAmt += 1
+		if GlobalVars.current_scene == "LevelGenerations":
+			if body.memoryAmt == 1:
+				pass # instantiate first scene
+			elif body.memoryAmt == 2:
+				pass
+			elif body.memoryAmt == 3:
+				pass
+			elif body.memoryAmt == 4:
+				pass
+		queue_free()
