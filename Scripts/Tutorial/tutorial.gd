e2fab51630d64341d55e90f027c1364effdf86a9
IgniFyris <ignifyris@gmail.com>
2026-04-13 22:21:44 +0800

    feat: finish tutorial functionality

---
+++ Scripts/Tutorial/tutorial.gd
@@ -0,0 +1,68 @@
+extends Node2D
+
+var balloon = load("uid://b4vdtsl8wg12c")
+var dialogue = load("uid://b05mgkw3ad0yw")
+
+var tween
+
+# Barriers
+@onready var MoveJump = $Barriers/Movement_and_Jump/CollisionShape2D
+@onready var BDash = $Barriers/Dash/CollisionShape2D
+
+# Areas
+@onready var TutEnd = $TutAreas/TutEnd
+@onready var Jump = $TutAreas/Jump
+@onready var ADash = $TutAreas/Dash
+@onready var Form = $TutAreas/Form
+
+# Labels
+@onready var LMove = $Labels/Move
+@onready var LJump = $Labels/Jump
+@onready var LDash = $Labels/Dash
+@onready var LForm = $Labels/Form
+
+func _ready() -> void:
+	await get_tree().create_timer(2.0).timeout
+	
+	DialogueManager.show_dialogue_balloon_scene(balloon, dialogue, "start")
+	
+	tween = create_tween().tween_property(LMove, "modulate:a", 1, 1)
+	GlobalVars.movement_jump = true
+	MoveJump.disabled = true
+	
+func _on_form_body_entered(body: Node2D) -> void:
+	if body is Player:
+		LForm.modulate.a = 0
+		tween = create_tween().tween_property(LForm, "modulate:a", 1, 1)
+		Form.set_deferred("monitoring", false)
+		BDash.set_deferred("disabled", false)
+		DialogueManager.show_dialogue_balloon_scene(balloon, dialogue, "forms")
+		
+		await DialogueManager.dialogue_ended
+		
+		GlobalVars.form = true
+
+func _on_dash_body_entered(body: Node2D) -> void:
+	if body is Player:
+		LJump.modulate.a = 0
+		tween = create_tween().tween_property(LDash, "modulate:a", 1, 1)
+		ADash.set_deferred("monitoring", false)
+		MoveJump.set_deferred("disabled", false)
+		DialogueManager.show_dialogue_balloon_scene(balloon, dialogue, "jumpdash")
+		
+		await DialogueManager.dialogue_ended
+		
+		GlobalVars.dash = true
+
+
+func _on_jump_body_entered(body: Node2D) -> void:
+	if body is Player:
+		LMove.modulate.a = 0
+		tween = create_tween().tween_property(LJump, "modulate:a", 1, 1)
+		Jump.set_deferred("monitoring", true)
+
+
+func _on_tut_end_body_entered(body: Node2D) -> void:
+	if body is Player:
+		TutEnd.set_deferred("monitoring", false)
+		SceneLoader.load_scene("uid://bjhbxh7kbf02h", 1)
--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -3,17 +3,18 @@ extends CanvasLayer
 var form = "none"
 
 func _process(_delta):
-	if Input.is_action_just_pressed("transformation"):
-		$SelectionWheel.show()
-		if Engine.time_scale == 1:
-			Engine.time_scale = 0.1
-		get_parent().get_parent().velocity.y = 0
-		get_viewport().warp_mouse(get_viewport().get_visible_rect().size / 2)
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	elif Input.is_action_just_released("transformation"):	
-		@warning_ignore("shadowed_variable_base_class")
-		var transform = $SelectionWheel.Close()
-		form = transform
-		if Engine.time_scale == 0.1:
-			Engine.time_scale = 1
-		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	if GlobalVars.form == true:
+		if Input.is_action_just_pressed("transformation"):
+			$SelectionWheel.show()
+			if Engine.time_scale == 1:
+				Engine.time_scale = 0.1
+			get_parent().get_parent().velocity.y = 0
+			get_viewport().warp_mouse(get_viewport().get_visible_rect().size / 2)
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		elif Input.is_action_just_released("transformation"):	
+			@warning_ignore("shadowed_variable_base_class")
+			var transform = $SelectionWheel.Close()
+			form = transform
+			if Engine.time_scale == 0.1:
+				Engine.time_scale = 1
+			Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
